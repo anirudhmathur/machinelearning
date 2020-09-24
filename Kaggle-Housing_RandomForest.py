3ca1ca12840fcb6c6c43bb22aed08a73fcdd3bb3
anirudhmathur <anirudh.mathur@gmail.com>
2020-09-24 20:50:08 +0530

    remove a feature

--- Kaggle-Housing_RandomForest.py
+++ Kaggle-Housing_RandomForest.py
@@ -10,9 +10,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+import seaborn as sns
 
 df = pd.read_csv("train.csv")
 print(df.head())
+
+
 
 #print all features
 features = []
@@ -50,7 +54,7 @@ print()
 print(df.describe(include= 'all').T)
 
 # Printing a specific column
-print(df['SalePrice'][:7]) # Printing top 7 rows
+#print(df['SalePrice'][:7]) # Printing top 7 rows
 
 print('-'*50)
 
@@ -61,10 +65,10 @@ print(df.iloc[10:14,-3:]) # The last three features and 10 to 13 rows
 print('-'*50)
 
 # Printing some rows and cols using specific cols name
-print(df.loc[90:95,'SalePrice'])
+#print(df.loc[90:95,'SalePrice'])
 
 # Lets see some row by applying condition
-print(df.loc[(df.SaleType == 'New') & (df.YrSold == 2007) & (df.MSZoning=='FV')])
+#print(df.loc[(df.SaleType == 'New') & (df.YrSold == 2007) & (df.MSZoning=='FV')])
 
 #features which have missing values
 for col in features:
@@ -126,9 +130,24 @@ print(type(X))
 y = df.iloc[:,-1].values
 print(type(y))
 
+# Plot scatter plot of SalePrice vs. GrLivArea
+plt.figure(figsize=(10,8))
+sns.set(style='darkgrid')
+sns.scatterplot(df['GrLivArea'], df['SalePrice'])
+plt.axvline(x=4000, c='r', linewidth=2)
+plt.title('Scatter Plot of Sale Price vs. Ground Living Area')
+plt.show()
+
+# We can now drop those rows
+df = df.drop(df[df['GrLivArea'] > 4000].index)
+print(df.shape)
+
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)
+
+
+regressor = RandomForestRegressor(n_estimators = 100, random_state = 0)
 regressor.fit(X_train, y_train)
 print("regressor_score is ",regressor.score(X_train,y_train))
 
@@ -137,7 +156,21 @@ np.set_printoptions(precision=2)
 print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
 r2score = r2_score(y_test, y_pred)
 print("r2_score is   ",r2score)
-#exit()
+
+#parameters = [{'C': [0.25, 0.5, 0.75, 1], 'kernel': ['linear']},
+ #             {'C': [0.25, 0.5, 0.75, 1], 'kernel': ['rbf'], 'gamma': [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]}]
+#grid_search = GridSearchCV(estimator = regressor,
+ #                          param_grid = parameters,
+  #                         scoring = 'accuracy',
+   #                        cv = 10,
+    #                       n_jobs = -1)
+#grid_search.fit(X_train, y_train)
+#best_accuracy = grid_search.best_score_
+#best_parameters = grid_search.best_params_
+#print("Best Accuracy: {:.2f} %".format(best_accuracy*100))
+#print("Best Parameters:", best_parameters)
+
+exit()
 
 ############################Test Data**************************************
 df_test = pd.read_csv("test.csv")
